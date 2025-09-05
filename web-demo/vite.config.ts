94e2a03e5f977aa1a34a1dace0a56a0b7248f2f3
OO <oo@OOdeMac-mini.local>
2025-09-05 18:14:16 +0800

    build: remove unused basicSsl import to fix TS6133

--- web-demo/vite.config.ts
+++ web-demo/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import { copyFileSync, mkdirSync } from 'fs'
 import { resolve } from 'path'
 
